cb8d3965020eac7e548cbbac52754e770c8e4274
wdomitrz <witekdomitrz@gmail.com>
2020-04-01 10:14:15 +0200

    Added tests

--- LM2.hs
+++ LM2.hs
@@ -108,8 +108,9 @@ data Exp = EInt Int
     | EOp  Op Exp Exp
     | EVar Var
     | ELet Var Exp Exp  -- let var = e1 in e2
+    deriving Show
 
-data Op = OpAdd | OpMul | OpSub
+data Op = OpAdd | OpMul | OpSub deriving Show
 type State = Map Var Int
 {- | evalExp
 >>> let test = ELet "x" (ELet "y" (EOp OpAdd (EInt 6) (EInt 9)) (EOp OpSub y (EInt 1))) (EOp OpMul x (EInt 3)) where x = EVar "x"; y = EVar "y"
@@ -154,9 +155,14 @@ renumberTree :: Tree a -> Tree Int
 renumberTree = flip evalState 0 . renumberTreeM
 
 -- b.
-data BExp = BTrue | BFalse | BNot BExp | BOp BOp BExp BExp | BCmp Cmp Exp Exp
-data BOp = BAnd | BOr
-data Cmp = Eq | Lt | Leq | Gt | Geq | Neq
+data BExp = BTrue
+          | BFalse
+          | BNot BExp
+          | BOp BOp BExp BExp
+          | BCmp Cmp Exp Exp
+          deriving Show
+data BOp = BAnd | BOr deriving Show
+data Cmp = Eq | Lt | Leq | Gt | Geq | Neq deriving Show
 evalBExp :: BExp -> Bool
 evalBExp = flip ($) empty . evalBExpM
 
@@ -189,9 +195,11 @@ evalBExpM (BCmp c e1 e2) = do
 
 data Stmt = SSkip -- skip
           | SAssign Var Exp -- x := e
-          | SSemicolon Stmt Stmt -- S1 ; S2
+          | SNext Stmt Stmt -- S1 ; S2
           | SIf BExp Stmt Stmt -- if b then S1 else S2
           | SWhile BExp Stmt -- while b do S
+          deriving Show
+
 execStmt :: Stmt -> IO ()
 execStmt = print . evalStmt
 
@@ -205,52 +213,15 @@ evalStmtM (SAssign v e) = do
     let n = evalExpM e s
     modify (insert v n)
     get
-evalStmtM (SSemicolon s1 s2) = do
+evalStmtM (SNext s1 s2) = do
     evalStmtM s1
     evalStmtM s2
 evalStmtM (SIf b i1 i2) = do
     s <- get
     let b' = evalBExpM b s
     evalStmtM (if b' then i1 else i2)
-evalStmtM w@(SWhile b i) = evalStmtM (SIf b (SSemicolon i w) SSkip)
+evalStmtM w@(SWhile b i) = evalStmtM (SIf b (SNext i w) SSkip)
 
--- This test is based on https://www.mimuw.edu.pl/~klin/teaching/sem19-20/Tiny.hs
-{-- Calculate the n+1'th Fibonacci number
-x := 0; y := 1; i := 0;
-while i<=n do
-  tmp := x+y;
-  x := y;
-  y := tmp;
-  i := i+1;
---}
-fibStmt :: Stmt
-fibStmt = SSemicolon
-    (SAssign "x" (EInt 0))
-    (SSemicolon
-        (SAssign "y" (EInt 1))
-        (SSemicolon
-            (SAssign "i" (EInt 0))
-            (SWhile
-                (BCmp Leq (EVar "i") (EVar "n"))
-                (SSemicolon
-                    (SAssign "tmp" (EOp OpAdd (EVar "x") (EVar "y")))
-                    (SSemicolon
-                        (SAssign "x" (EVar "y"))
-                        (SSemicolon
-                            (SAssign "y" (EVar "tmp"))
-                            (SAssign "i" (EOp OpAdd (EVar "i") (EInt 1)))
-                        )
-                    )
-                )
-            )
-        )
-    )
-fib :: Int -> Int
-fib n = (flip evalState (Map.singleton "n" n) . evalStmtM) fibStmt ! "x"
-{- | fib
->>> map fib [1 .. 30] == map fibs [1 .. 30]
-True
--}
 
 -- c.
 
@@ -258,7 +229,7 @@ data Decl = Decl Var Exp
 
 data Stmt' = SSkip' -- skip
            | SAssign' Var Exp -- x := e
-           | SSemicolon' Stmt' Stmt' -- S1 ; S2
+           | SNext' Stmt' Stmt' -- S1 ; S2
            | SIf' BExp Stmt' Stmt' -- if b then S1 else S2
            | SWhile' BExp Stmt' -- while b do S
            | SDecl [Decl] Stmt' -- begin [D] S end
@@ -288,8 +259,6 @@ evalDeclM (Decl v e) = do
     let l = alloc s
     return (insert v l rho, insert l (evalExpM e (rho +++ s)) s)
 
--- TODO
--- evalStmt''M :: (MonadState Store m, MonadReader VEnv m') => Stmt' -> m' (m Store)
 evalStmt'M :: MonadState Store m => Stmt' -> VEnv -> m Store
 evalStmt'M SSkip'         rho = get
 evalStmt'M (SAssign' v e) rho = do
@@ -297,21 +266,121 @@ evalStmt'M (SAssign' v e) rho = do
     let n = evalExpM e (rho +++ s)
     put (insert (rho ! v) n s)
     get
-evalStmt'M (SSemicolon' i1 i2) rho = do
+evalStmt'M (SNext' i1 i2) rho = do
     evalStmt'M i1 rho
     evalStmt'M i2 rho
 evalStmt'M (SIf' b i1 i2) rho = do
     s <- get
     let b' = evalBExpM b (rho +++ s)
     evalStmt'M (if b' then i1 else i2) rho
-evalStmt'M w@(SWhile' b i) rho =
-    evalStmt'M (SIf' b (SSemicolon' i w) SSkip') rho
-evalStmt'M (SDecl ds i) rho = do
+evalStmt'M w@(SWhile' b  i) rho = evalStmt'M (SIf' b (SNext' i w) SSkip') rho
+evalStmt'M (  SDecl   ds i) rho = do
     s <- get
     let (rho', s') = foldl (flip evalDeclM) (rho, s) ds
     put s'
     evalStmt'M i rho'
 
+
+-- Bigger tests
+-- 3
+-- b
+
+-- This test is based on https://www.mimuw.edu.pl/~klin/teaching/sem19-20/Tiny.hs
+{-- Calculate the n+1'th Fibonacci number
+x := 0; y := 1; i := 0;
+while i<=n do
+  tmp := x+y;
+  x := y;
+  y := tmp;
+  i := i+1;
+--}
+fibStmt :: Stmt
+fibStmt = SNext
+    (SAssign "x" (EInt 0))
+    (SNext
+        (SAssign "y" (EInt 1))
+        (SNext
+            (SAssign "i" (EInt 0))
+            (SWhile
+                (BCmp Leq (EVar "i") (EVar "n"))
+                (SNext
+                    (SAssign "tmp" (EOp OpAdd (EVar "x") (EVar "y")))
+                    (SNext
+                        (SAssign "x" (EVar "y"))
+                        (SNext (SAssign "y" (EVar "tmp"))
+                               (SAssign "i" (EOp OpAdd (EVar "i") (EInt 1)))
+                        )
+                    )
+                )
+            )
+        )
+    )
+fib :: Int -> Int
+fib n = (flip evalState (Map.singleton "n" n) . evalStmtM) fibStmt ! "x"
+{- | fib
+>>> map fib [1 .. 30] == map fibs [1 .. 30]
+True
+-}
+
+-- | Tests created by Mr. Artur Zaroda <zaroda@mimuw.edu.pl>
+-- >>> execStmt test1
+-- fromList [("i",0),("s",2432902008176640000)]
+-- >>> execStmt test2
+-- fromList [("a",37),("b",0),("p",0)]
+
+-- i = 20;
+-- s = 1;
+-- while (i != 0) {
+--     s = s * i;
+--     i = i - 1;
+-- }
+--
+test1 :: Stmt
+test1 = SNext
+    (SAssign "i" $ EInt 20)
+    (SNext
+        (SAssign "s" $ EInt 1)
+        (SWhile
+            (BCmp Neq i $ EInt 0)
+            (SNext (SAssign "s" $ EOp OpMul s i)
+                   (SAssign "i" $ EOp OpSub i (EInt 1))
+            )
+        )
+    )
+  where
+    s = EVar "s"
+    i = EVar "i"
+
+-- a = 2368;
+-- b = 3145;
+-- while (b) {
+--     p = a - b;
+--     if (p) {
+--         a = p;
+--     } else {
+--         b = 0 - p;
+--     }
+-- }
+--
+test2 :: Stmt
+test2 = SNext
+    (SAssign "a" $ EInt 2368)
+    (SNext
+        (SAssign "b" $ EInt 3145)
+        (SWhile
+            (BCmp Neq (EVar "b") $ EInt 0)
+            (SNext
+                (SAssign "p" $ EOp OpSub (EVar "a") (EVar "b"))
+                (SIf (BCmp Gt (EVar "p") $ EInt 0)
+                     (SAssign "a" $ EVar "p")
+                     (SAssign "b" $ EOp OpSub (EInt 0) (EVar "p"))
+                )
+            )
+        )
+    )
+
+
+-- c
 
 -- This test is based on https://www.mimuw.edu.pl/~klin/teaching/sem19-20/Declarations.hs
 -- EXAMPLE: BASIC SCOPING --
@@ -330,11 +399,11 @@ end
 fooStmt :: Stmt'
 fooStmt = SDecl
     [Decl "x" (EInt 123), Decl "y" (EInt 456)]
-    (SSemicolon'
+    (SNext'
         (SDecl
             [Decl "x" (EInt 789)]
-            (SSemicolon' (SAssign' "x" (EInt 7))
-                         (SAssign' "y" (EOp OpAdd (EVar "x") (EInt 1)))
+            (SNext' (SAssign' "x" (EInt 7))
+                    (SAssign' "y" (EOp OpAdd (EVar "x") (EInt 1)))
             )
         )
         (SAssign' "x" (EOp OpAdd (EVar "y") (EInt 1)))
